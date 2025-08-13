1324a39061829d5aeea63252e85dd6a7b6274dc3
Suhas Martha <suhasmartha@gmail.com>
2025-08-13 19:50:20 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mdx from '@mdx-js/rollup';
 import tailwindcss from "@tailwindcss/vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
