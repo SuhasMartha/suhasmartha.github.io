fcdc78bc088980017a107fcaaefcaed9bafb58c6
Suhas <suhasmartha@gmail.com>
2025-08-24 12:10:50 +0530

    update

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = "https://maagvnyxarmbzozufqcd.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hYWd2bnl4YXJtYnpvenVmcWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MTc4MzAsImV4cCI6MjA3MTA5MzgzMH0.DaUp1QSIWGfS29d_40NycU_vMpJvWK9reO4QnkLtBns";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase
